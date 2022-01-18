59a5f4d32217ee151ef2d25e26ed75004f72ffcb
jcfdelpino <juan.delpino@kellogg.northwestern.edu>
2022-01-18 16:10:54 +0000

    in class exercise 1

--- crm/crm.sql
+++ crm/crm.sql
@@ -1,5 +1,24 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS clients;
+DROP TABLE IF EXISTS sales_team;
+DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS activities;
 
 -- CREATE TABLES
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  industry_id INTEGER
+);
+CREATE TABLE clients (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT,
+  address TEXT,
+  company_id INTEGER
+);
