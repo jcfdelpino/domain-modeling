6cd295bf96873de7cc9ca6df567535261a32dfd9
jcfdelpino <juan.delpino@kellogg.northwestern.edu>
2022-01-18 17:20:21 +0000

    class 3 end

--- crm/crm.sql
+++ crm/crm.sql
@@ -1,24 +1,55 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
-DROP TABLE IF EXISTS companies;
-DROP TABLE IF EXISTS clients;
+DROP TABLE IF EXISTS company;
+DROP TABLE IF EXISTS client;
 DROP TABLE IF EXISTS sales_team;
-DROP TABLE IF EXISTS industries;
-DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS industry;
+DROP TABLE IF EXISTS activity;
+DROP TABLE IF EXISTS industry_membership;
 
 -- CREATE TABLES
-CREATE TABLE companies (
+
+CREATE TABLE company (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT,
   industry_id INTEGER
 );
-CREATE TABLE clients (
+
+CREATE TABLE client (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name TEXT,
   last_name TEXT,
+  position TEXT,
   email TEXT,
   phone_number TEXT,
   address TEXT,
   company_id INTEGER
+);
+
+CREATE TABLE sales_team (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
+
+CREATE TABLE industry (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+CREATE TABLE activity (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  type TEXT,
+  time TEXT,
+  notes TEXT,
+  sales_id INTEGER,
+  client_id  INTEGER
+);
+
+CREATE TABLE industry_membership (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  company_id INTEGER,
+  industry_id INTEGER
 );
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,21 @@
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS industry_memberships;
 
+
+CREATE TABLE post (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  time_stamp TEXT,
+  username_id TEXT,
+  comment_id TEXT
+);
+
+CREATE TABLE User (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT,
+  first_name TEXT,
+  last_name TEXT
+);
